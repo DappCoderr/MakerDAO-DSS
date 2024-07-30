8bf4312cd71023a71cbf10d4d1ca6eb1a743164b
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-07-30 18:34:33 +0530

    Update the deployDss script

--- script/DeployDSS.s.sol
+++ script/DeployDSS.s.sol
@@ -5,14 +5,28 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {DecentralizedStableCoin} from "src/DSC.sol";
 import {DecentralisedStableCoinSystem} from "src/DSS.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployDSS is Script{
+
+    address[] public tokenAddress;
+    address[] public priceFeedAddress;
+
     function run() external returns(DecentralizedStableCoin, DecentralisedStableCoinSystem) {
+
+        HelperConfig config = new HelperConfig();
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) = config.activeNetworkConfig();
+
+        tokenAddress = [weth,wbtc];
+        priceFeedAddress = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+
         vm.startBroadcast();
         DecentralizedStableCoin dsc = new DecentralizedStableCoin();
-        DecentralisedStableCoinSystem dss = new DecentralisedStableCoinSystem();
+        DecentralisedStableCoinSystem dss = new DecentralisedStableCoinSystem(tokenAddress, priceFeedAddress, address(DecentralizedStableCoin));
+        DecentralizedStableCoin.transferOwnership(address(DecentralisedStableCoinSystem));
         vm.stopBroadcast();
-        return (dsc, dss);
+        return (DecentralizedStableCoin, DecentralisedStableCoinSystem);
     }
 }
 
