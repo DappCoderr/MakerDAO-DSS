1861266d4a8ea3025ffcdf3137ec6af5fbae501e
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-07-30 18:34:49 +0530

    Udpate HelperConfig script

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -21,10 +21,17 @@ contract HelperConfig is Script{
     uint8 public constant DECIMALS = 8;
     int256 public constant ETH_USD_PRICE = 2000e8;
     int256 public constant BTC_USD_PRICE = 1000e8;
+    uint256 public constant DEFAULT_ANVIL_PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
 
     NetworkConfig public activeNetworkConfig;
 
-    constructor(){}
+    constructor(){
+        if(block.chainId == 11155111){
+            activeNetworkConfig = getSepoliaEthConfig();
+        }else{
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
 
     function getSepoliaEthConfig() public view returns(NetworkConfig memory){
         return NetworkConfig({
