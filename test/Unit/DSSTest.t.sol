364f67d6c5cfafc2a3c0c77793e0de5528af7ab9
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-08-06 18:00:15 +0530

    Setup Unit test

---
+++ test/Unit/DSSTest.t.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployDSS} from "script/DeployDSS.s.sol";
+import {DecentralizedStableCoin} from "src/DSC.sol";
+import {DecentralisedStableCoinSystem} from "src/DSS.sol";
+
+contract DSSTest is Test{
+
+}
