11037f6ee813bd7518c36260e8bd9dfb0943c785
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-08-06 18:27:00 +0530

    Add testCollateral

--- test/Unit/DSSTest.t.sol
+++ test/Unit/DSSTest.t.sol
@@ -7,6 +7,7 @@ import {DeployDSS} from "script/DeployDSS.s.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
 import {DecentralizedStableCoin} from "src/DSC.sol";
 import {DecentralisedStableCoinSystem} from "src/DSS.sol";
+import {ERC20Mock} from "lib/openzeppelin-contracts/contracts/mocks/token/ERC20Mock.sol";
 
 contract DSSTest is Test{
 
@@ -17,10 +18,15 @@ contract DSSTest is Test{
     address ethUsdPriceFeed;
     address weth;
 
+    address public USER = makeAddr("Alice");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+
     function setUp() public{
         deployer = new DeployDSS();
         (dsc, dss, config) = deployer.run();
         (ethUsdPriceFeed,,weth,,) = config.activeNetworkConfig();
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
     }
 
     function getUSDValue() public{
@@ -28,5 +34,13 @@ contract DSSTest is Test{
         uint256 expectedPrice = 30000e18;
         uint256 actualPrice = dss.getUsdValue(weth, ethAmount);
         assertEq(expectedPrice, actualPrice);
+    }
+
+    function testRevertIfCollateralIsZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dss), AMOUNT_COLLATERAL);
+        vm.expectRevert(DecentralisedStableCoinSystem.DSC_NeedMoreThanZero.selector);
+        dss.depositCollateral(weth, 0);
+        vm.stopPrank();
     }
 }
