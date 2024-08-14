29e9730b03d72f4ddd378d478b982e86a7cff09b
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-08-14 16:50:47 +0530

    Add OpenInvariants test

--- test/Fuzz/OpenInvariantsTest.t.sol
+++ test/Fuzz/OpenInvariantsTest.t.sol
@@ -1,21 +1,14 @@
 // SPDX-License-Identifier:MIT
 
-// Have our invariant aka properties
+pragma solidity ^0.8.19;
 
-// What are our invariants?
-
-// Total supply of our DSC tokne shour be less than our collateral value
-
-// Getter view functions should never revert <- evergreen invariant
-
-pragma solidity ^0.8.18;
-
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
 import {DeployDSS} from "script/DeployDSS.s.sol";
 import {DecentralisedStableCoinSystem} from "src/DSS.sol";
 import {DecentralizedStableCoin} from "src/DSC.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract OpenInvariantTest is StdInvariant, Test {
 
@@ -23,11 +16,25 @@ contract OpenInvariantTest is StdInvariant, Test {
     DecentralizedStableCoin dsc;
     DecentralisedStableCoinSystem dss;
     HelperConfig config;
+    address weth;
+    address wbtc;
 
     function setUp() external {
         deployerDss = new DeployDSS();
         (dsc, dss, config) = deployerDss.run();
+        (,,weth, wbtc,) = config.activeNetworkConfig(); 
         targetContract(address(dss));
+    }
+
+    function invariant_protocolMustHaveMoreValueThanTotalSupply(){
+        uint256 totalSupply = dsc.totalSupply();
+        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dss));
+        uint256 totalWbtcDeposited = IERC20(wbtc).balanceOf(address(dss));
+
+        uint256 wethValue = dss.getUsdValue(weth, totalWethDeposited);
+        uint256 wbtcValue = dss.getUsdValue(wbtc, totalWethDeposited);
+
+        assert(wethValue + wbtcValue >= totalSupply);
     }
 }
 
