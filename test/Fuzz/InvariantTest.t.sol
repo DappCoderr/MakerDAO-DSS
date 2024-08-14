947de4b0d3c45ef652ea8eaabf3c4ed69bb63c73
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-08-14 18:16:35 +0530

    Update invariant test

---
+++ test/Fuzz/InvariantTest.t.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeployDSS} from "script/DeployDSS.s.sol";
+import {DecentralizedStableCoin} from "src/DSC.sol";
+import {DecentralisedStableCoinSystem} from "src/DSS.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+
+contract InvariantTest is Test, StdInvariant{
+
+    DeployDSS deployer;
+    DecentralizedStableCoin dsc;
+    DecentralisedStableCoinSystem dss;
+    HelperConfig config;
+    
+    function setUp() external {
+        deployer = new DeployDSS()
+        (dsc, dss, config) = deployer.run();
+        (,,weth, wbtc,) = config.activeNetworkConfig();
+        targetContract(address(dss));
+    }
+}
